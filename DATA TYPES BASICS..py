96ae02f8386de315f2292b98a4f3b680419de2c2
VANDANKUMAR LODHA <96713580+VandanLodha@users.noreply.github.com>
2021-12-27 12:01:07 +0530

    Add files via upload

---
+++ DATA TYPES BASICS..py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+#integer,string,float,complex TYPES OF DATA.
+
+#integer(it has numerical value.)
+a = 10
+type(a)
+
+
+# In[9]:
+
+
+#string(It has alphabetical data.)
+b = 'data'
+type(b)
+
+
+# In[13]:
+
+
+#float(It has decimal values.)
+c = 10.0
+type(c)
+
+
+# In[14]:
+
+
+#complex
+d = 2+9j
+type(d)
+
+
+# In[15]:
+
+
+#LIST,SET,TUPLE,DICTIONARY 
+
+#LIST(It contains all types of data within [] bracket.)
+e = ['analysis',10,12.2]
+type(e)
+
+
+# In[16]:
+
+
+#SET(It contains all types of the data within {} bracket.)
+f = {'analysis',10,12.2}
+type(f)
+
+
+# In[17]:
+
+
+#TUPLE(It has all data types within () bracket.)
+g = ('analysis',10 ,12.2)
+type(g)
+
+
+# In[22]:
+
+
+#DICTIONARY(It contains two types of data that is a key and a value within{} bracket.)
+h = {'key1':'data','key2':10,'key4':12.2}
+type(h)
+
+
+# In[ ]:
+
+
+
+
